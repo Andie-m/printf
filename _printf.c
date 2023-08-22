34dd6154ee46bc6a0e1e29d3f93c4e8015d542d0
Andie-m <sabinakanario@gmail.com>
2023-08-22 10:03:23 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,66 +1,66 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <main.h>
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - Custom printf function with limited format specifiers.
- * @format: The format string containing directives.
- * @...: Variable number of and for the directives.
- *
- * This function prints output according to the provided format string,
- * handling the following conversion specifiers: %c, %s, and %%.
- *
- * Return: The number of characters printed (excluding the null byte used to end output to strings).
+ * _printf - the Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-va_list and;
-va_start(and, format);
-int akm = 0;
-while (*format != '\0')
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-putchar(va_arg(and, int));
-akm++;
-break;
-case 's':
-{
-const char *str = va_arg(and, const char *);
-while (*str != '\0')
-{
-putchar(*str);
-str++;
-akm++;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
-break;
-}
-case '%':
-putchar('%');
-akm++;
-break;
-}
-}
-else
-{
-putchar(*format);
-akm++;
-}
-format++;
-}
-va_end(and);
-return (akm);
-}
+
 /**
- * main - this main
- * Return:(0) success
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
-int main(void)
+void print_buffer(char buffer[], int *buff_ind)
 {
-_printf("Hello, %s! The answer is %d%c\n", "World", 42, '%');
-printf("akls printf: %d\n", akm);
-return (0);
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
