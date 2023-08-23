5aa0a8ca4eb2215ee5456c75429d3722cd84cfbc
Andie-m <sabinakanario@gmail.com>
2023-08-23 02:00:54 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,46 +1,75 @@
 #include "main.h"
-#include <stdarg.h>
 
-int _printf(const char *format, ...) {
-    va_list and;
-    va_start(and, format);
+/*
+ * _printf = my own printf
+ * @format = format string
+ * Return: number of characters printed to standard output
+ **/
 
-    int akm = 0;
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'c':
-                    putchar(va_arg(and, int));
-                    akm++;
-                    break;
-                case 's': 
-                {
-                    const char str = va_arg(and, const char );
-                    while (*str != '\0') {
-                        putchar(*str);
-                        str++;
-                        akm++;
-                    }
-                    break;
-                }
-                case '%':
-                    putchar('%');
-                    akm++;
-                    break;
-            }
-        } else {
-            putchar(*format);
-            akm++;
-        }
-        format++;
-    }
+int _printf(const char *format, ...)
+{
 
-    va_end(and);
-    return akm;
-}
+	int char_print = 0;
 
-int main() {
-    _printf("Hello, %s! The answer is %d%c\n", "World", 42, '%');
-    return 0;
+	va_list arg_list;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(arg_list, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			char_print++;
+		}
+		else
+		{
+			format++;
+			if (*format  == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				char_print++;
+			}
+
+			else if (*format == 'c')
+			{
+				char c = va_arg(arg_list, int);
+
+				write(1, &c, 1);
+				char_print++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(arg_list, char*);
+
+				if (str == NULL)
+				{
+					write(1, "(null)", 6);
+					char_print += 6;
+				}
+				else
+				{
+					while (*str)
+					{
+						write(1, str, 1);
+						char_print++;
+						str++;
+					}
+				}
+			}
+			else
+			{
+				write(1, format - 1, 2);
+				char_print += 2;
+			}
+		}
+		format++;
+	}
+	va_end(arg_list);
+	return (char_print);
 }
