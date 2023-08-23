223c53e902a173dd0ad38cf4b96c3f87b4389736
Andie-m <sabinakanario@gmail.com>
2023-08-23 01:57:55 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,94 +1,46 @@
 #include "main.h"
+#include <stdarg.h>
 
-/*
-* _printf = my own printf
-* @format = format string
-* Return: number of characters printed to standard output
-**/
+int _printf(const char *format, ...) {
+    va_list and;
+    va_start(and, format);
 
-int _printf(const char *format, ...)
-{
-int char_print = 0;
-va_list arg_list;
+    int akm = 0;
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c':
+                    putchar(va_arg(and, int));
+                    akm++;
+                    break;
+                case 's': 
+                {
+                    const char str = va_arg(and, const char );
+                    while (*str != '\0') {
+                        putchar(*str);
+                        str++;
+                        akm++;
+                    }
+                    break;
+                }
+                case '%':
+                    putchar('%');
+                    akm++;
+                    break;
+            }
+        } else {
+            putchar(*format);
+            akm++;
+        }
+        format++;
+    }
 
-if (format == NULL)
-return (-1);
-
-va_start(arg_list, format);
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-if (*format  == '\0')
-break;
-char_print += handle_format(format, arg_list);
-}
-else
-{
-write(1, format, 1);
-char_print++;
-}
-format++;
-}
-va_end(arg_list);
-return (char_print);
-}
-
-/*
-* handle_format = handle different format specifiers
-* @format = pointer to the current character in the format string
-* @arg_list = list of arguments
-* Return: number of characters printed for the current specifier
-**/
-
-int handle_format(const char *format, va_list arg_list)
-{
-switch (*format)
-{
-case '%':
-write(1, format, 1);
-return (1);
-case 'c':
-return (print_char(va_arg(arg_list, int)));
-case 's':
-return (print_string(va_arg(arg_list, char*)));
-default:
-write(1, format - 1, 2);
-return (2);
-}
+    va_end(and);
+    return akm;
 }
 
-/*
-* print_char = print a single character
-* @c = character to print
-* Return: number of characters printed (always 1)
-**/
-
-int print_char(char c)
-{
-write(1, &c, 1);
-return (1);
-}
-
-/*
-* print_string = print a string or "(null)" if NULL
-* @str = string to print
-* Return: number of characters printed
-**/
-
-int print_string(char *str)
-{
-int len = 0;
-
-if (str == NULL)
-str = "(null)";
-
-while (*str)
-{
-write(1, str, 1);
-len++;
-str++;
-}
-return (len);
+int main() {
+    _printf("Hello, %s! The answer is %d%c\n", "World", 42, '%');
+    return 0;
 }
